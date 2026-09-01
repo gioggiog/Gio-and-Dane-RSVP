49af99e55104f38863f712f37c59bfc2596cdf35
gioggiog <gio.tibon@gmail.com>
2026-09-01 13:25:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -81,3 +81,31 @@ if (navToggle && navMenu) {
         });
     });
 }
+
+// Aesthetic 3D Coverflow Carousel Initialization
+const gallerySwiper = new Swiper('.gallery-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    coverflowEffect: {
+        rotate: 30,       // Slide angle rotation in degrees
+        stretch: 0,        // Stretch space between slides
+        depth: 100,        // Depth offset (3D perspective depth)
+        modifier: 1,       // Effect multiplier
+        slideShadows: false, // Set to true if you want subtle dark 3D shadows
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
